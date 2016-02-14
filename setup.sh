7f36990edf41345945f1532948d6ec8af8487eab
Jason Frey <fryguy9@gmail.com>
2016-02-13 23:13:56 -0500

    Add iTerm2 Preferences

--- setup.sh
+++ setup.sh
@@ -3,16 +3,17 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 for target in \
-	bin \
-	"Library/Application Support/Sublime Text 3/Packages/ManageIQ Log" \
-	"Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings" \
-	"Library/Application Support/Sublime Text 3/Packages/User/Twilight (Fryguy).tmTheme" \
 	.gemrc \
 	.gitconfig \
 	.gitignore_global \
 	.irbrc \
 	.profile \
-	.pryrc
+	.pryrc \
+	bin \
+	Documents/dotfiles \
+	"Library/Application Support/Sublime Text 3/Packages/ManageIQ Log" \
+	"Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings" \
+	"Library/Application Support/Sublime Text 3/Packages/User/Twilight (Fryguy).tmTheme"
 do
   echo "Linking $target"
   ln -sfh "$DIR/$target" ~/"$target"

52a0c86d4b41a58025a07a5b347b37b0b48a69db
Jason Frey <fryguy9@gmail.com>
2016-02-13 22:39:43 -0500

    Add SublimeText theme.

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 for target in \
 	bin \
 	"Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings" \
+	"Library/Application Support/Sublime Text 3/Packages/User/Twilight (Fryguy).tmTheme" \
 	.gemrc \
 	.gitconfig \
 	.gitignore_global \

e58d18fda8a2c9242879fcd88f8fa644103c0956
Jason Frey <fryguy9@gmail.com>
2020-03-16 16:29:58 -0400

    Add DefaultKeyBinding for Home/End key
    - Also support creating dirs if they don't exist before linking

--- setup.sh
+++ setup.sh
@@ -13,8 +13,12 @@ for target in \
 	.ruby_tools.rb \
 	bin \
 	"Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings" \
-	"Library/Application Support/Sublime Text 3/Packages/User/Twilight (Fryguy).tmTheme"
+	"Library/Application Support/Sublime Text 3/Packages/User/Twilight (Fryguy).tmTheme" \
+	"Library/KeyBindings/DefaultKeyBinding.dict"
 do
+  link_dir="$(dirname ~/"$target")"
+  [ ! -d "$link_dir" ] && echo "Creating $link_dir" && mkdir -p "$link_dir"
+
   echo "Linking $target"
   ln -sfh "$DIR/$target" ~/"$target"
 done

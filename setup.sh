f63e3c2c14a3b5b434d938282f4b7ba68c9f4335
Jason Frey <fryguy9@gmail.com>
2020-03-23 22:37:49 -0400

    Add linking of SublimeText settings

--- setup.sh
+++ setup.sh
@@ -12,15 +12,19 @@ for target in \
 	.pryrc \
 	.ruby_tools.rb \
 	bin \
-	"Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings" \
-	"Library/Application Support/Sublime Text 3/Packages/User/Twilight (Fryguy).tmTheme" \
 	"Library/KeyBindings/DefaultKeyBinding.dict"
 do
-  link_dir="$(dirname ~/"$target")"
-  [ ! -d "$link_dir" ] && echo "Creating $link_dir" && mkdir -p "$link_dir"
+	link_dir="$(dirname ~/"$target")"
+	[ ! -d "$link_dir" ] && echo "Creating $link_dir" && mkdir -p "$link_dir"
 
-  echo "Linking $target"
-  ln -sfh "$DIR/$target" ~/"$target"
+	echo "Linking $target"
+	ln -sfh "$DIR/$target" ~/"$target"
+done
+
+IFS=$'\n' files=($(ls "Sublime Text 3/Packages/User"))
+for target in ${files[@]}; do
+	echo "Linking $target"
+	ln -sfh "$DIR/Sublime Text 3/Packages/User/$target" ~/"Library/Application Support/Sublime Text 3/Packages/User/$target"
 done
 
 mkdir -p ~/Library/Preferences/bat/themes

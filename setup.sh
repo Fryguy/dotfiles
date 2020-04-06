7ff149a4f70803b1265169cffc8bc9bba0a15534
Jason Frey <fryguy9@gmail.com>
2020-04-05 21:32:52 -0400

    Allow setup for both mac and linux

--- setup.sh
+++ setup.sh
@@ -2,6 +2,22 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	IS_MAC=true
+fi
+
+function link_file() {
+	file="$1"
+	target="$2"
+
+	link_dir=$(dirname "$target")
+	[ ! -d "$link_dir" ] && echo "Creating $link_dir" && mkdir -p "$link_dir"
+
+	echo "Linking $target -> $file"
+	ln -sf "$file" "$target"
+}
+
+
 for target in \
 	.bundler.d/Gemfile.global.rb \
 	.gemrc \
@@ -14,21 +30,24 @@ for target in \
 	.profile \
 	.pryrc \
 	.ruby_tools.rb \
-	bin \
-	Library/KeyBindings/DefaultKeyBinding.dict
+	bin
 do
-	link_dir="$(dirname ~/"$target")"
-	[ ! -d "$link_dir" ] && echo "Creating $link_dir" && mkdir -p "$link_dir"
-
-	echo "Linking $target"
-	ln -sfh "$DIR/$target" ~/"$target"
+	link_file "$DIR/$target" ~/"$target"
 done
+
+if [ "$IS_MAC" == "true" ]; then
+	target="Library/KeyBindings/DefaultKeyBinding.dict"
+	link_file "$DIR/$target" ~/"$target"
+
+	link_file "$DIR/Sublime Text 3/Packages/User/Twilight (Fryguy).tmTheme" ~/"Library/Preferences/bat/themes/Twilight (Fryguy).tmTheme"
+fi
 
 IFS=$'\n' files=($(ls "Sublime Text 3/Packages/User"))
 for target in ${files[@]}; do
-	echo "Linking $target"
-	ln -sfh "$DIR/Sublime Text 3/Packages/User/$target" ~/"Library/Application Support/Sublime Text 3/Packages/User/$target"
+	if [ "$IS_MAC" == "true" ]; then
+		target_dir=~/"Library/Application Support"
+	else
+		target_dir="/mnt/c/Users/Fryguy/AppData/Roaming"
+	fi
+	link_file "$DIR/Sublime Text 3/Packages/User/$target" "$target_dir/Sublime Text 3/Packages/User/$target"
 done
-
-mkdir -p ~/Library/Preferences/bat/themes
-ln -sfh "$DIR/Library/Application Support/Sublime Text 3/Packages/User/Twilight (Fryguy).tmTheme" ~/"Library/Preferences/bat/themes/Twilight (Fryguy).tmTheme"

8c34de57de26696c55fd6c8ddd404e278ba8ad41
Jason Frey <fryguy9@gmail.com>
2020-08-08 15:31:04 -0400

    Fixed var substitution

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -4,7 +4,7 @@ set -e
 
 SOURCE_REPO=${SOURCE_REPO:-Fryguy/prezto}
 
-git clone --recursive git@github.com:SOURCE_REPO ~/.zprezto
+git clone --recursive git@github.com:$SOURCE_REPO ~/.zprezto
 cd ~/.zprezto
 git remote add upstream git@github.com:sorin-ionescu/prezto
 git checkout changes

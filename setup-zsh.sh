e02283654e3b4aa93d8396305d9258191dc64af9
Jason Frey <fryguy9@gmail.com>
2023-04-17 16:50:10 -0400

    More idempotent zsh setup

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -1,15 +1,22 @@
-#!/bin/bash
+#!/bin/zsh
 
 set -e
 
 SOURCE_REPO=${SOURCE_REPO:-Fryguy/prezto}
+DIR=$HOME/.zprezto
 
-git clone --recursive git@github.com:$SOURCE_REPO ~/.zprezto
-cd ~/.zprezto
-git remote add upstream git@github.com:sorin-ionescu/prezto
-git checkout changes
-git submodule update --init --recursive
+if [ ! -d $DIR ]; then
+	git clone --recursive git@github.com:$SOURCE_REPO $DIR
+	pushd $DIR
+		git remote add upstream git@github.com:sorin-ionescu/prezto
+		git checkout changes
+		git submodule update --init --recursive
+	popd
+fi
+
+cd $DIR
 setopt EXTENDED_GLOB
-for rcfile in ~/.zprezto/runcoms/^README.md(.N); do
-	ln -s "$rcfile" "~/.${rcfile:t}"
+for rcfile in $DIR/runcoms/^README.md(.N); do
+	echo "Linking $rcfile to $HOME/.${rcfile:t}"
+	ln -F -s "$rcfile" "$HOME/.${rcfile:t}"
 done

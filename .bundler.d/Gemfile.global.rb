70acbe970a3a1d5dbcbb4a0af60be4ac5c5cd347
Jason Frey <fryguy9@gmail.com>
2024-03-05 15:57:26 -0500

    Adjust based on Ruby version in use

--- .bundler.d/Gemfile.global.rb
+++ .bundler.d/Gemfile.global.rb
@@ -1,9 +1,18 @@
-ensure_gem "pry", "~> 0.13.x", "< 0.14"
-ensure_gem "pry-byebug"
-ensure_gem "pry-doc"
-ensure_gem "pry-rails"
-ensure_gem "pry-theme"
-ensure_gem "dead_end"
+#ensure_gem "dead_end"
 #ensure_gem "stackprof"
 #ensure_gem "rubyprof"
 #ensure_gem "rubyprof-flamegraph"
+
+if RUBY_VERSION >= "3.1.0"
+  ensure_gem "reline", ">= 0.3.7"
+end
+
+if RUBY_VERSION < "3.1"
+  ensure_gem "pry", "~> 0.13.x", "< 0.14"
+  ensure_gem "pry-byebug"
+  ensure_gem "pry-doc"
+  ensure_gem "pry-rails"
+  ensure_gem "pry-theme"
+end
+
+ensure_gem "syntax_suggest"
